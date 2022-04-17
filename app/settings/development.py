4da30c08ac8c03d93cbe2d7e81056ead9afb0f0c
Olivier <noreply4github@gmail.com>
2022-04-17 23:39:36 +0200

    disable the debug-toolbar when Debug is off

--- app/settings/development.py
+++ app/settings/development.py
@@ -1,5 +1,16 @@
+import environ
 from .common import *
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, 'settings', 'development.env'))
 
 DEBUG = True
 
+SECRET_KEY = env('SECRET_KEY')
+
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
+
+if DEBUG:
+    INSTALLED_APPS += ['debug_toolbar',]
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
+    INTERNAL_IPS = ['127.0.0.1',]
--- app/settings/production.py
+++
@@ -1,5 +0,0 @@
-from .common import *
-
-DEBUG = False
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
