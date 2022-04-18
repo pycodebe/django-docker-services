f7f1b86340d5bc3a49e6c4f283ac8b389bd7bc93
Olivier <noreply4github@gmail.com>
2022-04-18 11:51:50 +0200

    feature: rename app into demo and app_1 into app

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,11 @@
-from django.conf import settings
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'heroes', views.HeroViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('app_1/', include('app_1.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
-
-if settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include('debug_toolbar.urls')),
-    ]
