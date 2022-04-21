6e558efd9e70dfc73f7ef576f69cb50ed3c14a88
Olivier <noreply4github@gmail.com>
2022-04-21 10:54:45 +0200

    add a logging to the production settings

--- src/framework/settings/production.py
+++ src/framework/settings/production.py
@@ -1,4 +1,4 @@
-from os import environ
+from os import environ, path
 from .common import *
 
 DEBUG = False
@@ -6,3 +6,22 @@ DEBUG = False
 SECRET_KEY = environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': path.join(BASE_DIR, 'debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
