3a1d33c50a40427ed9cf726853de85a21318c7fc
Olivier <noreply4github@gmail.com>
2022-04-22 16:09:13 +0200

    add the nginx service as a reverse proxy

--- src/framework/settings/common.py
+++ src/framework/settings/common.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'fake_secret_key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
