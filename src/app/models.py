3a1fc97360cb0ff86e650df016c3e11d97476a3d
Olivier <noreply4github@gmail.com>
2022-05-26 19:27:26 +0200

    feature: simplify the structure of the projects and change the model with Car

--- src/app/models.py
+++ src/app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-class Hero(models.Model):
-    name = models.CharField(max_length=60)
-    alias = models.CharField(max_length=60)    
-    
+class Car(models.Model):
+    model = models.CharField(max_length=60)
+    brand = models.CharField(max_length=60)    
+
+    class Meta:
+        db_table = 'cars'
+
     def __str__(self):
-        return self.name
+        return self.model
